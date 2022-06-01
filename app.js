282f485e59f09bc3f59a3ddc893535f91d5dffab
SanazBHMN <sanazbahmani35@gmail.com>
2022-06-01 10:25:18 +0430

    Adding middleware

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require("http");
 
-const routes = require("./routes");
+const express = require("express");
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
